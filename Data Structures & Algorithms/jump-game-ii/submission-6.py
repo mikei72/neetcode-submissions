84282b871c640816f5bf48019400e8b453f548e6
mikei72 <144695712+mikei72@users.noreply.github.com>
2026-07-20 16:54:08 +0800

    Add: jump-game-ii - submission-6

---
+++ Data Structures & Algorithms/jump-game-ii/submission-6.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        pos = 0
+        count = 0
+        while pos < len(nums) - 1:
+            if pos + nums[pos] >= len(nums) - 1:
+                return count + 1
+
+            best = 0
+            best_idx = pos
+            for i in range(nums[pos]):
+                cur = pos + i + 1
+                if nums[cur] + cur >= best + best_idx:
+                    best = nums[cur]
+                    best_idx = cur
+            
+            pos = best_idx
+            count += 1
+            print(pos)
+
+        return count
