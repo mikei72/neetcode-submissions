3cf787dee4bb355d8cd1ef5e05e9c1a19d430ccb
mikei72 <144695712+mikei72@users.noreply.github.com>
2026-08-04 17:50:28 +0800

    Add: clone-graph - submission-8

---
+++ Data Structures & Algorithms/clone-graph/submission-8.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        map = {}
+
+        def dfs(n):
+            if n in map:
+                return map[n]
+
+            new = Node(n.val)
+            map[n] = new
+            for nei in n.neighbors:
+                new.neighbors.append(dfs(nei))
+            return new
+        
+        return dfs(node)
