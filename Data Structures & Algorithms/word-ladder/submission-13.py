951d4c2ba416832defe43a986b63e50b42f2c515
mikei72 <144695712+mikei72@users.noreply.github.com>
2026-08-16 14:28:44 +0800

    Add: word-ladder - submission-13

---
+++ Data Structures & Algorithms/word-ladder/submission-13.py
@@ -0,0 +1,29 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList or beginWord == endWord:
+            return 0
+
+        words, res = set(wordList), 0
+        q = deque([beginWord])
+
+        letters = set()
+        for w in wordList:
+            for c in w:
+                letters.add(c)
+
+        while q:
+            res += 1
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node == endWord:
+                    return res
+                
+                for i in range(len(node)):
+                    for c in letters:
+                        if c == node[i]:
+                            continue
+                        nei = node[:i] + c + node[i + 1:]
+                        if nei in words:
+                            q.append(nei)
+                            words.remove(nei)
+        return 0
